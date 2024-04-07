e04983f5177092908c58c7e9b532e0d1c04a0cd1
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2024-04-07 20:03:20 +0200

    gotowy client na 100%

--- client.py
+++ client.py
@@ -5,9 +5,17 @@ class AirVisualClient:
     def __init__(self):
         self.api_key = "873dffac-2098-49b7-b803-d3ccd5dae470"
         self.basic_url = "https://api.airvisual.com/v2/"
+        self.city = "Warsaw"
+        self.state = "Mazovia"
+        self.country = "Poland"
 
     def get_air_quality(self):
-        url = f"{self.basic_url}city?city=Warsaw&state=Mazovia&country=Poland&key={self.api_key}"
+        url = f"{self.basic_url}city?city={self.city}&state={self.state}&country={self.country}&key={self.api_key}"
         response = requests.get(url)
         data = response.json()
+        print(data)
         return data
+
+
+if __name__ == "__main__":
+    AirVisualClient().get_air_quality()
